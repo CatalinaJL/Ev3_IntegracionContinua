2ffa0d3761cfb8f2ca6edd88e2b3c898d1fb5b01
CatalinaJL <catalina.p.jl@gmail.com>
2024-09-15 18:58:45 -0300

    ADD: Se agrega archivo HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo{
+   public static void main(String[] args){
+       System.out.println("Hola Mundo");
+       System.out.println("Para la evaluacion 3 de Integracion Continua"); 
+   }
+
+}
+
